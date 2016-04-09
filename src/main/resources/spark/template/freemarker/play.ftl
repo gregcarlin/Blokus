667521355c4128d05c9e8a96e7d8557bfd910325
George Spahn <george_spahn@brown.edu>
2016-04-08 20:08:43 -0400

    Added Piece Dragging Functionality

--- src/main/resources/spark/template/freemarker/play.ftl
+++ src/main/resources/spark/template/freemarker/play.ftl
@@ -1,41 +1,10 @@
 <#assign content>
 
 <h1> PLAY BLOKUS </h1>
-<div id = "left">
-<canvas id = "board" width = "500" height = "500">
+<canvas id = "board" width = "1000" height = "500">
 </canvas>
-</div>
-<div> Piece-List: </div><br>
 
-<table id = "pieceList" border = "1">
-  <tr>
-    <td>I1</td>
-    <td>I2</td>		
-    <td>I3</td>
-    <td>V3</td>
-    <td>I4</td>		
-    <td>L4</td>
-    <td>O4</td>
-  </tr>
-  <tr>
-    <td>T4</td>
-    <td>Z4</td>		
-    <td>F5</td>
-    <td>I5</td>
-    <td>L5</td>		
-    <td>N5</td>
-    <td>P5</td>
-  </tr>
-  <tr>
-    <td>T5</td>
-    <td>U5</td>		
-    <td>V5</td>
-    <td>W5</td>
-    <td>X5</td>		
-    <td>Y5</td>
-    <td>Z5</td>
-  </tr>
-</table><br>
+<br>
 <button id = "rotate">Rotate</button><br>
 <button id = "flip">Flip</button><br>
 <button id = "cancel">Cancel</button><br>
--- src/main/resources/static/js/play.js
+++ src/main/resources/static/js/play.js
@@ -1,22 +1,38 @@
 board = document.getElementById("board");
 ctx = board.getContext("2d");
 SIZE = 25;
+supplyLeftEdge = 25*SIZE;
+size2 = Math.floor(SIZE/2);
 grid = []
-for (i = 0; i < 20; i++) {
-	grid[i] = [];
-	for (j = 0; j < 20; j++) {
-		grid[i][j] = 0;
-}}
+
+remainingPieces = [0,[],[],[],[]];
+
 curPlayer = 1;
 curPiece = 0;
 rotate = [1,0,0,1];
-mode = "relaxing";
+
+mode = "relaxing"; 
+//modes include "relaxing" -> "dragging" -> "positioning" 
 
 curPieceX = 0;
 curPieceY = 0;
 
 curMouseX = 0;
 curMouseY = 0;
+
+function init() {
+	var foo = [];
+	for (i = 0; i < 21; i++) {
+		foo[i] = 1;
+	}
+	remainingPieces = [0,foo.slice(0),foo.slice(0),
+			foo.slice(0),foo].slice(0);
+	for (i = 0; i < 20; i++) {
+	grid[i] = [];
+	for (j = 0; j < 20; j++) {
+		grid[i][j] = 0;
+}}
+}
 
 function orient(piece) {
 	var p = pieces[piece].slice(0);
@@ -31,45 +47,67 @@ function orient(piece) {
 	return p;
 }
 
-$("td").on('click', function() {
-	var tag = $(this).text();
-	curPiece = pieceVals[tag];
-	if (mode == "positioning") {
-		drawGrid();
-		drawCurPiece();
-		return;
-	}
-	mode = "placing";
-});
+
 
 function drawGrid() {
 	ctx.clearRect(0, 0, board.width, board.height);
+	ctx.fillStyle = colors[0];
+	ctx.fillRect(0,0,board.width,board.height);
 	ctx.beginPath();
-	for (i = 0;i<20;i++)
+	
+	for (i = 0;i<21;i++)
 	{
 	ctx.moveTo(SIZE*i,0);
 	ctx.lineTo(SIZE*i,20*SIZE);
 	ctx.moveTo(0,SIZE*i);
 	ctx.lineTo(20*SIZE,SIZE*i);
 	}
+	
 	ctx.stroke();
 
 	for (i=0;i<20;i++) {
 	for (j = 0;j<20;j++) {
-		fillSquare(grid[i][j],i,j);
+		fillGridSquare(grid[i][j],i,j);
 	}}
+	
+	drawSupply();
 }
 
-function fillSquare(player, x, y) {
-	ctx.fillStyle = colors[player];
-	ctx.fillRect(SIZE*x+1,20*SIZE-SIZE*(y+1)+1,SIZE-2,SIZE-2);
+function drawSupply() {
+	console.log(remainingPieces);
+	for (m = 0; m < 3; m++) {
+	for (n = 0; n < 7; n++) {
+		var piece = 3*n + m;
+		if (remainingPieces[curPlayer][piece] == 1) {
+			var xloc = supplyLeftEdge + (m*6+2)*size2;
+			var yloc = (n*6+2)*size2;
+			drawPiece(pieces[piece],curPlayer,xloc,yloc,size2);
+		}
+	}}
+	
+}
+
+function fillSquare(color,x,y,width) {  // x and y are bottom left corner
+	ctx.save();
+	ctx.fillStyle = color;
+	ctx.fillRect(x,board.height-y-width,width,width);
+	ctx.restore();
+}
+
+function fillGridSquare(player, x, y) {   //fills row x col y gridsquare with color of player
+	if (player == 0) return;
+	fillSquare(colors[player],SIZE*x,SIZE*y,SIZE,SIZE);
+}
+
+function drawPiece(piece,player,x,y,squareSize) {
+	for (i = 0; i < piece.length/2; i++) {
+		fillSquare(colors[player],x+squareSize*piece[2*i],
+			y+squareSize*piece[2*i+1],squareSize);
+	}
 }
 
 function drawCurPiece() {
-	var locs = orient(curPiece);
-	for (i = 0; i < locs.length/2; i++) {
-		fillSquare(curPlayer,curPieceX+locs[2*i],curPieceY+locs[2*i+1]);
-	}
+	drawPiece(orient(curPiece),curPlayer, SIZE*curPieceX,SIZE*curPieceY,SIZE);
 }
 
 
@@ -82,27 +120,68 @@ function getMousePos(canvas,evt) {
     };
 }
 
+$("td").on('click', function() {
+	var tag = $(this).text();
+	curPiece = pieceVals[tag];
+	if (mode == "positioning") {
+		drawGrid();
+		drawCurPiece();
+		return;
+	}
+	mode = "placing";
+});
+
 
 $("#board").mousemove(function(e){
 	
-	var col = Math.floor(getMousePos(board,e).x/SIZE);
-	var row = Math.floor((500-getMousePos(board,e).y)/SIZE);
-	curMouseX = col;
-	curMouseY = row;
+	//var col = Math.floor(getMousePos(board,e).x/SIZE);
+	//var row = Math.floor((500-getMousePos(board,e).y)/SIZE);
+	//curMouseX = col;
+	//curMouseY = row;
 	
-	if (mode != "placing") return;
-	curPieceX = col;
-	curPieceY = row;
-	drawGrid();
-	drawCurPiece();
+	//if (mode != "placing") return;
+	//curPieceX = col;
+	//curPieceY = row;
+	//drawGrid();
+	//drawCurPiece();
+	
+	curMouseX = getMousePos(board,e).x;
+	curMouseY = board.height - getMousePos(board,e).y;
+	if (mode == "dragging") {
+		curPieceX = Math.floor(curMouseX/SIZE);
+		curPieceY = Math.floor(curMouseY/SIZE);
+		drawGrid();
+		drawCurPiece();
+	}
+});
+
+$("#board").mousedown(function(e){
+	if (curMouseX > supplyLeftEdge && curMouseX < supplyLeftEdge + 3*6*size2) {
+		var supplyX = Math.floor((curMouseX - supplyLeftEdge)/(6*size2));
+		var supplyY = Math.floor(curMouseY/(6*size2));
+		var newPiece = supplyX + 3*supplyY;
+		if (remainingPieces[curPlayer][newPiece] == 0) return;
+		curPiece = newPiece;
+		curPieceX = Math.floor(curMouseX/SIZE);
+		curPieceY = Math.floor(curMouseY/SIZE);
+		drawGrid();
+		drawCurPiece();
+		mode = "dragging";
+	}
 });
 
 $("#board").mouseup(function(e){
-	if (mode == "placing") {
-		mode = "positioning";
-		$("button").show();
-		curPieceX = curMouseX;
-		curPieceY = curMouseY;
+	if (mode == "dragging") {
+		if (curMouseX < 20*SIZE) {
+			mode = "positioning";
+			$("button").show();
+			curPieceX = Math.floor(curMouseX/SIZE);
+			curPieceY = Math.floor(curMouseY/SIZE);
+		}
+		else {
+			mode = "relaxing";
+			drawGrid();
+		}
 	}
 });
 
@@ -112,7 +191,6 @@ $("#rotate").on('click', function() {
 	var temp2 = -1*rotate[0];
 	var temp3 = rotate[3];
 	var temp4 = -1*rotate[2];
-	
 	rotate = [temp1,temp2,temp3,temp4];
 	drawGrid();
 	drawCurPiece();
@@ -126,7 +204,6 @@ $("#flip").on('click', function() {
 	drawCurPiece();
 });
 
-
 $("#cancel").on('click', function() {
 	mode = "placing";
 	drawGrid();
@@ -139,6 +216,7 @@ $("#submit").on('click', function() {
 	for (i = 0; i < locs.length/2; i++) {
 		grid[curPieceX+locs[2*i]][curPieceY+locs[2*i+1]] = curPlayer;
 	}
+	remainingPieces[curPlayer][curPiece] = 0;
 	curPlayer = (curPlayer + 1) % 5;
 	if (curPlayer == 0) curPlayer++;
 
@@ -159,8 +237,11 @@ function getCookie(cname) {
 }
 
 $(document).ready(function(){
+	board.width = board.height*2;
+	init();
 	drawGrid();
 	$("button").hide();
+	$("#pieceList").hide();
 
   var conn = new WebSocket('ws://' + window.location.host + '/live');
   conn.onopen = function() {
