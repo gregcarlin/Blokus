29afe678812f435f8e9f231ea3b360909795671a
Aaron Zhang <aaronzhang1@brown.edu>
2016-04-23 17:13:57 -0400

    Fixed Game tests

--- src/test/java/edu/brown/cs/blokus/GameTest.java
+++ src/test/java/edu/brown/cs/blokus/GameTest.java
@@ -10,8 +10,6 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.BeforeClass;
 
-import edu.brown.cs.blokus.ai.TotalComponentSizeAI;
-
 /**
  * Tests for {@link Game}.
  */
@@ -57,6 +55,7 @@ public class GameTest {
       .player(Turn.FIRST, new Player("one1"))
       .player(Turn.SECOND, new Player("two1"))
       .player(Turn.THIRD, new Player("three"))
+      .player(Turn.FOURTH, new Player("four"))
       .build())
     .setBoard(board2).build();
     
@@ -81,7 +80,7 @@ public class GameTest {
     game3 = new Game.Builder()
     .setSettings(new GameSettings.Builder()
       .player(Turn.FIRST, new Player("one2"))
-      .player(Turn.SECOND, new Player("two2"))
+      .player(Turn.SECOND, new Player("two2", game1Shapes, 0, true))
       .player(Turn.THIRD, new Player("three2"))
       .player(Turn.FOURTH, new Player("fourth2"))
       .build())
@@ -91,23 +90,38 @@ public class GameTest {
     
     // game 4
     Board board4 = new Board();
+    board4.setXY(5, 15, 1);
+    board4.setXY(15, 15, 2);
+    board4.setXY(14, 14, 3);
+    board4.setXY(14, 16, 3);
+    board4.setXY(16, 14, 3);
+    board4.setXY(16, 16, 3);
     game4 = new Game.Builder()
     .setSettings(new GameSettings.Builder()
-      .player(Turn.FIRST, new Player("one3"))
-      .player(Turn.SECOND, new Player("two3"))
+      .player(Turn.FIRST, new Player("one", game1Shapes, 0, true))
+      .player(Turn.SECOND, new Player("two", game1Shapes, 0, true))
+      .player(Turn.THIRD, new Player("three", game1Shapes, 0, true))
+      .player(Turn.FOURTH, new Player("four"))
       .build())
     .setBoard(board4).build();
 
  // game 5
     Board board5 = new Board();
+    board5.setXY(10, 10, 2);
+    board5.setXY(9, 9, 3);
+    board5.setXY(9, 11, 3);
+    board5.setXY(11, 9, 3);
+    board5.setXY(15, 11, 3);
+    board5.setXY(11, 12, 3);
     game5 = new Game.Builder()
     .setSettings(new GameSettings.Builder()
-      .player(Turn.FIRST, new Player("one5"))
-      .player(Turn.SECOND, new Player("two5"))
+      .player(Turn.FIRST, new Player("one5", EnumSet.noneOf(Shape.class), 0, true))
+      .player(Turn.SECOND, new Player("two5", EnumSet.of(Shape.I5), 0, true))
       .player(Turn.THIRD, new Player("three5"))
       .player(Turn.FOURTH, new Player("four5"))
       .build())
-    .setBoard(board2).build();
+    .setBoard(board5)
+    .build();
     
     
   }
@@ -120,123 +134,78 @@ public class GameTest {
   @Test
   public void testIsLegal() {
 //    // Should be illegal: piece goes off edge of board
-//    assertFalse(game1.isLegal(new Move(Shape.F5, Orientation.E, 0, 0)));
+    assertFalse(game1.isLegal(new Move(Shape.F5, Orientation.E, 0, 0)));
 //    // Should be legal
-//    assertTrue(game1.isLegal(new Move(Shape.F5, Orientation.E, 3, 4)));
+    assertTrue(game1.isLegal(new Move(Shape.F5, Orientation.E, 3, 4)));
 //    // Should be illegal: piece is on an occupied square
-//    assertFalse(game1.isLegal(new Move(Shape.F5, Orientation.E, 1, 2)));
+    assertFalse(game1.isLegal(new Move(Shape.F5, Orientation.E, 1, 2)));
 //    // Should be illegal: piece touches own color at edge
-//    assertFalse(game1.isLegal(new Move(Shape.F5, Orientation.E, 10, 8)));
+    assertFalse(game1.isLegal(new Move(Shape.F5, Orientation.E, 10, 8)));
 //    // Should be illegal: piece doesn't touch own color at any corner
-//    assertFalse(game1.isLegal(new Move(Shape.F5, Orientation.E, 15, 15)));
+    assertFalse(game1.isLegal(new Move(Shape.F5, Orientation.E, 15, 15)));
 //    // Should be legal
-//    assertTrue(game1.isLegal(new Move(Shape.F5, Orientation.E, 11, 8)));
+    assertTrue(game1.isLegal(new Move(Shape.F5, Orientation.E, 11, 8)));
 //    // Should be legal
-//    assertTrue(game1.isLegal(new Move(Shape.N5, Orientation.S, 12, 11)));
+    assertTrue(game1.isLegal(new Move(Shape.N5, Orientation.S, 12, 11)));
     
     
 //  //GAME 2
-//    assertTrue(game2.isLegal(new Move(Shape.I5, Orientation.E, 2, 0)));
-//    assertFalse(game2.isLegal(new Move(Shape.I5, Orientation.E, 4, 3)));
-//    assertFalse(game2.isLegal(new Move(Shape.I5, Orientation.E, 7, 3)));
-//    assertFalse(game2.isLegal(new Move(Shape.I5, Orientation.E, 8, 9)));
+    assertTrue(game2.isLegal(new Move(Shape.I5, Orientation.E, 2, 19)));
+    assertFalse(game2.isLegal(new Move(Shape.I5, Orientation.E, 4, 3)));
+    assertFalse(game2.isLegal(new Move(Shape.I5, Orientation.E, 7, 3)));
+    assertFalse(game2.isLegal(new Move(Shape.I5, Orientation.E, 8, 9)));
 //
 //    //Game 3
-//    assertTrue(game3.isLegal(new Move(Shape.U5, Orientation.E, 11, 16)));
-//    assertTrue(game3.isLegal(new Move(Shape.P5, Orientation.E, 16, 10)));
-//    assertTrue(game3.isLegal(new Move(Shape.L4, Orientation.E, 12, 6)));
-//    assertTrue(game3.isLegal(new Move(Shape.L5, Orientation.E, 6, 5)));
-//    assertFalse(game3.isLegal(new Move(Shape.Z4, Orientation.E, 8, 8)));
-//    assertFalse(game3.isLegal(new Move(Shape.O4, Orientation.E, 9, 9)));
-//    assertFalse(game3.isLegal(new Move(Shape.I5, Orientation.E, 15,8)));
+    assertTrue(game3.isLegal(new Move(Shape.U5, Orientation.E, 11, 16)));
+    assertTrue(game3.isLegal(new Move(Shape.P5, Orientation.E, 16, 10)));
+    assertTrue(game3.isLegal(new Move(Shape.L4, Orientation.E, 12, 6)));
+    assertFalse(game3.isLegal(new Move(Shape.L5, Orientation.E, 6, 5)));
+    assertFalse(game3.isLegal(new Move(Shape.Z4, Orientation.E, 8, 8)));
+    assertFalse(game3.isLegal(new Move(Shape.O4, Orientation.E, 9, 9)));
+    assertFalse(game3.isLegal(new Move(Shape.I5, Orientation.E, 15,8)));
   }
 
   @Test
   public void testCanMove() {
 //	    // Game 1
-//      assertTrue(game1.canMove(Turn.FIRST));
-//      assertFalse(game1.canMove(Turn.SECOND));
-//      assertFalse(game1.canMove(Turn.THIRD));
-//      assertFalse(game1.canMove(Turn.FOURTH));
-//      System.out.println(game1.canMove(Turn.FIRST));
-//      System.out.println(game1.canMove(Turn.SECOND));
-//      System.out.println(game1.canMove(Turn.THIRD));
-//      System.out.println(game1.canMove(Turn.FOURTH));
-//      
-//      // Game 2
-//      assertTrue(game2.canMove(Turn.FIRST));
-//      assertFalse(game2.canMove(Turn.SECOND));
-//      assertFalse(game2.canMove(Turn.THIRD));
-//      assertFalse(game2.canMove(Turn.FOURTH));
-//
-//      // Game 3 
-//      assertTrue(game3.canMove(Turn.SECOND));
-//      assertFalse(game3.canMove(Turn.FIRST));
-//      assertFalse(game3.canMove(Turn.THIRD));
-//      assertFalse(game3.canMove(Turn.FOURTH));
+      assertTrue(game1.canMove(Turn.FIRST));
+      assertTrue(game1.canMove(Turn.SECOND));
+      assertTrue(game1.canMove(Turn.THIRD));
+      assertTrue(game1.canMove(Turn.FOURTH));
+      
+      assertTrue(game4.canMove(Turn.FIRST));
+      assertFalse(game4.canMove(Turn.SECOND));
+      assertTrue(game4.canMove(Turn.THIRD));
+      assertTrue(game4.canMove(Turn.FOURTH));
+      
+      assertFalse(game5.canMove(Turn.FIRST));
+      assertFalse(game5.canMove(Turn.SECOND));
+      assertTrue(game5.canMove(Turn.THIRD));
+      assertTrue(game5.canMove(Turn.FOURTH));
   }
   
   @Test
   public void testMakeMove() {
-	  // Game 1
-	    assertTrue(game1.canMove(Turn.SECOND));
-	    game1.makeMove(new Move(Shape.F5, Orientation.E, 3, 4));
-	    assertTrue(game1.canMove(Turn.THIRD));
-
-//	  // Game 2
-//	    assertTrue(game2.canMove(Turn.FIRST));
-//	    game2.makeMove(new Move(Shape.I5, Orientation.E, 2, 0));
-//	    assertFalse(game2.canMove(Turn.FIRST));
-//	    assertTrue(game2.canMove(Turn.SECOND));
-//
-//	  // Game 3
-//	  assertTrue(game3.canMove(Turn.SECOND));
-//	  game3.makeMove(new Move(Shape.P5, Orientation.E, 11, 4));
-//	  assertFalse(game3.canMove(Turn.SECOND));
-//	  assertFalse(game3.canMove(Turn.THIRD));
-
-  }
-
-  @Test
-  public void testPass() {
-
-  }
-
-  
-  
-  /**
-   * Simulates game by picking random moves.
-   */
-  public static void simulateGame() {
-    Game g = new Game.Builder().setSettings(new GameSettings.Builder()
-      .player(Turn.FIRST, new Player("1"))
-      .player(Turn.SECOND, new Player("2"))
-      .player(Turn.THIRD, new Player("3"))
-      .player(Turn.FOURTH, new Player("4")).build()).build();
-    while (g.getTurn() != null) {
-      g.makeMove(g.getRandomMove(g.getTurn()));
-    }
-    System.out.println(g.getBoard());
-    System.out.println(g.getAllPlayers().stream()
-      .map(p -> p.getScore()).collect(Collectors.toList()));
-  }
-  
-  public static void simulateGameSmart() {
-    AI ai = new TotalComponentSizeAI();
-    Game g = new Game.Builder().setSettings(new GameSettings.Builder()
-      .player(Turn.FIRST, new Player("1"))
-      .player(Turn.SECOND, new Player("2"))
-      .player(Turn.THIRD, new Player("3"))
-      .player(Turn.FOURTH, new Player("4")).build()).build();
-    while (g.getTurn() != null) {
-      g.makeMove(ai.suggestMove(g));
-    }
-    System.out.println(g.getBoard());
-    System.out.println(g.getAllPlayers().stream()
-      .map(p -> p.getScore()).collect(Collectors.toList()));
-  }
-  
-  public static void main(String[] args) {
-    simulateGameSmart();
+	  Board board6 = new Board();
+    Game game6 = new Game.Builder()
+    .setSettings(new GameSettings.Builder()
+      .player(Turn.FIRST, new Player("one1"))
+      .player(Turn.SECOND, new Player("two1"))
+      .player(Turn.THIRD, new Player("three"))
+      .player(Turn.FOURTH, new Player("four"))
+      .build())
+    .setBoard(board6).build();
+    assertEquals(board6.getXY(0, 19), 0);
+    assertEquals(board6.getXY(19, 19), 0);
+    assertEquals(board6.getXY(19, 0), 0);
+    assertEquals(board6.getXY(0, 0), 0);
+    game6.makeMove(game6.getRandomMove(game6.getTurn()));
+    assertEquals(board6.getXY(0, 19), 1);
+    game6.makeMove(game6.getRandomMove(game6.getTurn()));
+    assertEquals(board6.getXY(19, 19), 2);
+    game6.makeMove(game6.getRandomMove(game6.getTurn()));
+    assertEquals(board6.getXY(19, 0), 3);
+    game6.makeMove(game6.getRandomMove(game6.getTurn()));
+    assertEquals(board6.getXY(0, 0), 4);
   }
 }
